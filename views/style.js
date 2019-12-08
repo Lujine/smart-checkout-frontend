ebc56a6554c214ad41d91142a10e5e3066eef9ba
mostafa-elhaiany <mostafa_elhaiany@outlook.com>
2019-12-08 14:23:01 +0200

    feat barcode get item

---
+++ views/style.js
@@ -0,0 +1,13 @@
+const React = require("react-native");
+
+const { StyleSheet } = React;
+
+export default {
+
+cancelButton: {
+  backgroundColor: 'red',
+  borderRadius: 5,
+  height: 45,
+  marginTop: 10,
+},
+};
